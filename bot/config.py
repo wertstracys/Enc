26b6f08166bc78338f1239e977e0783eb2d84fe5
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-02-27 21:20:03 +0000

    pyLint: auto-fixes

--- bot/config.py
+++ bot/config.py
@@ -60,7 +60,10 @@ class Config:
             self.FINISHED_PROGRESS_STR = config("FINISHED_PROGRESS_STR", default="🧡")
             self.FL_CAP = config("FILENAME_AS_CAPTION", default=False, cast=bool)
             self.FS_THRESHOLD = config("FLOOD_SLEEP_THRESHOLD", default=600, cast=int)
-            self.FSTICKER = config("FSTICKER", default="CAACAgUAAx0CbRJSKwABAmBIZ7PIZ2qqbl6d1ActhLacXmwH-oYAAhoDAAIX8Dw_8wpGlQuEOsM2BA")
+            self.FSTICKER = config(
+                "FSTICKER",
+                default="CAACAgUAAx0CbRJSKwABAmBIZ7PIZ2qqbl6d1ActhLacXmwH-oYAAhoDAAIX8Dw_8wpGlQuEOsM2BA",
+            )
             self.LOCK_ON_STARTUP = config("LOCK_ON_STARTUP", default=False, cast=bool)
             self.LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
             self.LOGS_IN_CHANNEL = config("LOGS_IN_CHANNEL", default=True, cast=bool)
